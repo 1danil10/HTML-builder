986d2dd11bfacd5b8ad174f926949daa6e7f0add
1danil10 <danilmatvienko@gmail.com>
2021-11-03 16:59:36 +0300

    fix: rewrite task 03

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,26 +1,45 @@
 const { stdout } = process;
 const fs = require('fs');
 const path = require('path');
+const {
+  colorStringForOutput,
+  colorErrorMessage,
+} = require('../utils/colorConsoleText');
 
 const folder = 'secret-folder';
 
-fs.readdir(path.join(__dirname, folder), 'utf8', (err, files) => {
-  if (err) throw err;
-  files.forEach((file) => {
-    fs.stat(path.join(__dirname, folder, file), (err, stats) => {
-      if (stats.isFile()) {
-        const [name, ext] = file.split(/\.(?=[0-9a-z]+$)/);
+function getFolderFiles(folderPath) {
+  fs.readdir(folderPath, 'utf8', (err, files) => {
+    if (err) {
+      throw new Error(colorErrorMessage(`Can't read folder: \n ${err}`));
+    }
 
-        stdout.write(
-          `${name.padEnd(8, ' ')} --- ${ext.padEnd(4, ' ')} --- ${bytes2Kb(
-            stats.size,
-          ).toFixed(2)}kb \n`,
-        );
-      }
+    files.forEach((file) => {
+      getFileInfo(folderPath, file, showFileInfo);
     });
   });
-});
-
-function bytes2Kb(value) {
-  return value / 1024;
 }
+
+function getFileInfo(folderPath, fileName, cb) {
+  fs.stat(path.join(folderPath, fileName), (err, stats) => {
+    if (err) {
+      throw new Error(colorErrorMessage(`Error while reading file:\n ${err}`));
+    }
+
+    if (!stats.isFile()) {
+      return false;
+    }
+
+    const [name, ext] = fileName.split(/\.(?=[0-9a-z]+$)/);
+    cb({ name, ext, size: stats.size });
+  });
+}
+
+function showFileInfo({ name, ext, size }) {
+  const formattedName = colorStringForOutput('blue', name.padEnd(10, ' '));
+  const formattedExt = colorStringForOutput('yellow', ext.padEnd(5, ' '));
+  const formattedSize = colorStringForOutput('cyan', size.toString());
+  stdout.write(`${formattedName}${formattedExt}${formattedSize + 'bytes'}\n`);
+}
+
+getFolderFiles(path.join(__dirname, folder));
