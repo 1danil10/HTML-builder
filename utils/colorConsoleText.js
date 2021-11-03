54595ce98ab36de77bc2460bcc0ff7bb89d20ee1
1danil10 <danilmatvienko@gmail.com>
2021-11-03 08:19:39 +0300

    feat: create function to color console output(only textcolor)

---
+++ utils/colorConsoleText.js
@@ -0,0 +1,29 @@
+const TEXT_COLORS = [
+  { name: 'white', code: [37, 89] },
+  { name: 'blue', code: [34, 89] },
+  { name: 'yellow', code: [33, 89] },
+  { name: 'red', code: [31, 89] },
+  { name: 'cyan', code: [36, 89] },
+  { name: 'green', code: [32, 89] },
+  { name: 'magenta', code: [35, 89] },
+  { name: 'gray', code: [30, 89] },
+];
+const whiteTextColor = TEXT_COLORS.find((el) => el.name === 'white');
+
+function colorStringForOutput(textColor, string) {
+  const {
+    code: [textColorStart, textColorEnd],
+  } =
+    TEXT_COLORS.find((el) => el.name === textColor.toLowerCase()) ??
+    whiteTextColor;
+
+  return `\x1b[${textColorStart}m${string}\x1b[${textColorEnd}m`;
+}
+
+const colorSuccessMessage = (str) =>
+  colorStringForOutput.call(null, 'green', str);
+const colorErrorMessage = (str) => colorStringForOutput.call(null, 'red', str);
+
+module.exports.colorStringForOutput = colorStringForOutput;
+module.exports.colorSuccessMessage = colorSuccessMessage;
+module.exports.colorErrorMessage = colorErrorMessage;
